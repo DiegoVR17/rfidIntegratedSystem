2ccd3150c6fbb0a8bfdf43a2e4c05c5279ade832
DiegoVR17 <diego.velez1@udea.edu.co>
2023-11-02 19:37:41 -0500

    commit door control funcional en db

---
+++ app/src/main/java/com/example/rfid_integrated_system_app/data/DoorControlRepository.kt
@@ -0,0 +1,34 @@
+package com.example.rfid_integrated_system_app.data
+
+import android.content.ContentValues
+import android.util.Log
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
+
+class DoorControlRepository {
+
+    private var db = Firebase.firestore
+    suspend fun open_close_door(estadoSwitch: HashMap<String, Boolean>): ResourceRemote<String?>{
+        return try {
+            db.collection("state_door")
+                .document("open_door")
+                .set(estadoSwitch).await()
+            ResourceRemote.Success(data = "ok")
+        } catch (e: FirebaseFirestoreException){
+            Log.e("FirebaseAuthException",e.localizedMessage)
+            ResourceRemote.Error(message = e.localizedMessage)
+        } catch (e: FirebaseNetworkException){
+            Log.e("FirebaseNetworkException",e.localizedMessage)
+            ResourceRemote.Error(message = e.localizedMessage)
+        }
+        catch (e: FirebaseException){
+            Log.e("FirebaseException",e.localizedMessage)
+            ResourceRemote.Error(message = e.localizedMessage)
+        }
+
+    }
+}
--- app/src/main/java/com/example/rfid_integrated_system_app/ui/door_control/DoorControlViewModel.kt
+++ app/src/main/java/com/example/rfid_integrated_system_app/ui/door_control/DoorControlViewModel.kt
@@ -1,18 +1,52 @@
 package com.example.rfid_integrated_system_app.ui.door_control
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.rfid_integrated_system_app.data.DoorControlRepository
+import com.example.rfid_integrated_system_app.data.ResourceRemote
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.launch
 
 class DoorControlViewModel: ViewModel() {
+
+    val db = Firebase.firestore
+    val doorControlRepository = DoorControlRepository()
     val _open_close_Msg: MutableLiveData<String?> = MutableLiveData()
     val open_close_Msg: LiveData<String?> = _open_close_Msg
 
     fun open_close_fun(checked: Boolean) {
-        if (checked){
-            _open_close_Msg.postValue("Puerta abierta")
-        }else{
-            _open_close_Msg.postValue("Puerta cerrada")
+        val estadoSwitch = hashMapOf(
+            "state_door" to checked
+        )
+
+        viewModelScope.launch {
+            var result =  doorControlRepository.open_close_door(estadoSwitch)
+            result.let { resourceRemote ->
+                when(resourceRemote){
+                    is ResourceRemote.Success -> {
+                        if (checked){
+                            _open_close_Msg.postValue("Puerta abierta")
+                        }else{
+                            _open_close_Msg.postValue("Puerta cerrada")
+                        }
+                    }
+                    is ResourceRemote.Error -> {
+                        var msg = result.message
+                        when(msg){
+                            "A network error (such as timeout, interrupted connection or unreachable host) has occurred." -> msg = "Revise su conexión a internet"
+                        }
+                        _open_close_Msg.postValue(msg)
+                    }
+                    else ->{
+
+                    }
+                }
+            }
         }
     }
 
